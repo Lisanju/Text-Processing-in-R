36e120127f602140409161b188efec6ad6445418
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-08-12 15:50:43 -0300

    Update script.r

--- script.r
+++ script.r
@@ -1,1 +1,15 @@
+setwd("home/ufscar/ProjetoElisa")
 
+ubirajara <- scan(file="TextosInput/JAUbirajara.txt", what="character", sep="\n", strip.white = TRUE)
+
+head(ubirajara, 10)
+
+vetorUbirajara <- paste(ubirajara, collapse = " ")
+length(vetorUbirajara)
+
+ubirajaraSentencas <- stringr::str_split_1(vetorUbirajara, pattern = "[\u2014.;!?]+")
+
+palavrasChave <- c("sozinho", "sozinha", "sozinhos", "sozinhas")
+sentencasFiltradas <- ubirajaraSentencas[sapply(ubirajaraSentencas, function(s) any(grepl(paste(palavrasChave, collapse="|"), s)))]
+
+matrizSentencasFiltradas <- matrix(sentencasFiltradas, ncol=1)
