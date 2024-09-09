226d73efb3bd6a8b9f6a30a6866e2e50e0c3b800
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-09-09 10:23:37 -0300

    Update script.r

--- script.r
+++ script.r
@@ -1,13 +1,39 @@
+library(stringr)
+library(tokenizers)
+
 # Base de dados: https://www.gutenberg.org/browse/languages/pt
 
 # Diretório
-setwd("")
+setwd(" ")
 
-# Processamento
-arquivo <- scan(file="URL", what="character", sep="\n", strip.white = TRUE)
-texto <- paste(arquivo, collapse = " ")
-sentencas <- stringr::str_split_1(texto, pattern = "[\u2014.;!?]+")
+# Processamento do texto
+textoGutenberg <- scan(file="https://www.gutenberg.org/ebooks/69229.txt.utf-8", what="character", sep="\n", strip.white = FALSE, blank.lines.skip = F)
 
-# Filtragem
-palavras <- c("sozinho", "sozinha", "sozinhos", "sozinhas")
-sentencasFiltradas <- sentencas[grepl(paste(palavras, collapse = "|"), sentencas, ignore.case = TRUE)]
+# Delimitar início e fim do texto
+inicio <- which(str_detect(textoGutenberg, pattern = "START OF THE PROJECT GUTENBERG EBOOK") == T) + 1
+fim <- which(str_detect(textoGutenberg, pattern = "END OF THE PROJECT GUTENBERG EBOOK") == T) - 1
+textoGutenberg <- textoGutenberg[inicio:fim]
+
+# Juntar linhas que não são quebras de parágrafo
+x <- textoGutenberg
+y <- paste(x, collapse = " ")
+z <- gsub(pattern = "  ", replacement = "\n", y)
+write(z, "~/textoTratado.txt")
+
+textoTratado <- readLines("~/textoTratado.txt")
+
+# Filtragem de palavras no texto
+pal <- tokenize_words(x = textoTratado)
+pal <- unlist(pal)
+
+ocorPal <- which(str_detect(pal, "s[oóò]zinh[aos]") == T)
+
+# Filtragem de sentenças no texto
+sen <- tokenize_sentences(x = textoTratado)
+sen <- unlist(sen)
+
+ocorSen <-which(str_detect(sen, "s[oóò]zinh[aos]") == T)
+
+# Transposição das sentenças para um dataframe
+dataSen <- data.frame()
+dataSen <- rbind(dataSen, data.frame(sentenças = sen[ocorSen]))
