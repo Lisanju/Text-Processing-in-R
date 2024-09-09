dfb161c87e594bfcec91cea611a53d9b517a2620
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-09-09 10:54:45 -0300

    Update script.r

--- script.r
+++ script.r
@@ -3,11 +3,25 @@ library(tokenizers)
 
 # Base de dados: https://www.gutenberg.org/browse/languages/pt
 
+# Textos da amostra A (Pré-1990)
+# 1 - https://www.gutenberg.org/cache/epub/63438/pg63438.txt
+# 2 - https://www.gutenberg.org/cache/epub/69239/pg69239.txt
+# 3 - https://www.gutenberg.org/cache/epub/69229/pg69229.txt
+# 4 - https://www.gutenberg.org/cache/epub/67594/pg67594.txt
+# 5 - https://www.gutenberg.org/cache/epub/69187/pg69187.txt
+
+# Textos da amostra B (Pós-1990)
+# 1 - 
+# 2 - 
+# 3 - 
+# 4 - 
+# 5 - 
+
 # Diretório
 setwd("/home/lisanju/Desktop/ProjetoElisa")
 
 # Processamento do texto [ALTERE AQUI O TEXTO QUE DEVE SER PROCESSADO]
-textoGutenberg <- scan(file="https://www.gutenberg.org/cache/epub/69239/pg69239.txt", what="character", sep="\n", strip.white = FALSE, blank.lines.skip = F)
+textoGutenberg <- scan(file="https://www.gutenberg.org/cache/epub/69229/pg69229.txt", what="character", sep="\n", strip.white = FALSE, blank.lines.skip = F)
   
 # Delimitar início e fim do texto
 inicio <- which(str_detect(textoGutenberg, pattern = "START OF THE PROJECT GUTENBERG EBOOK") == T) + 1
@@ -36,4 +50,4 @@ ocorSen <-which(str_detect(sen, "s[oóò]zinh[aos]") == T)
 
 # Transposição das sentenças para um dataframe
 dataSen <- data.frame(tipo = NULL, sentenças = NULL)
-dataSen <- rbind(dataSen, data.frame(tipo = "A", sentenças = sen[ocorSen]))
+dataSen <- rbind(dataSen, data.frame(amostra = "A", sentenças = sen[ocorSen]))
