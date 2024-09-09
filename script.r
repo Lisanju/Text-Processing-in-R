a53e47cbaf913deeddc3e1746c9d1186c9071e68
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-09-09 10:50:07 -0300

    Update script.r

--- script.r
+++ script.r
@@ -4,30 +4,30 @@ library(tokenizers)
 # Base de dados: https://www.gutenberg.org/browse/languages/pt
 
 # Diretório
-setwd(" ")
+setwd("/home/lisanju/Desktop/ProjetoElisa")
 
-# Processamento do texto
-textoGutenberg <- scan(file="https://www.gutenberg.org/ebooks/69229.txt.utf-8", what="character", sep="\n", strip.white = FALSE, blank.lines.skip = F)
-
+# Processamento do texto [ALTERE AQUI O TEXTO QUE DEVE SER PROCESSADO]
+textoGutenberg <- scan(file="https://www.gutenberg.org/cache/epub/69239/pg69239.txt", what="character", sep="\n", strip.white = FALSE, blank.lines.skip = F)
+  
 # Delimitar início e fim do texto
 inicio <- which(str_detect(textoGutenberg, pattern = "START OF THE PROJECT GUTENBERG EBOOK") == T) + 1
 fim <- which(str_detect(textoGutenberg, pattern = "END OF THE PROJECT GUTENBERG EBOOK") == T) - 1
 textoGutenberg <- textoGutenberg[inicio:fim]
-
+  
 # Juntar linhas que não são quebras de parágrafo
 x <- textoGutenberg
 y <- paste(x, collapse = " ")
 z <- gsub(pattern = "  ", replacement = "\n", y)
-write(z, "~/textoTratado.txt")
-
-textoTratado <- readLines("~/textoTratado.txt")
-
+write(z, "/home/lisanju/Desktop/ProjetoElisa/textoTratado.txt")
+  
+textoTratado <- readLines("/home/lisanju/Desktop/ProjetoElisa/textoTratado.txt")
+  
 # Filtragem de palavras no texto
 pal <- tokenize_words(x = textoTratado)
 pal <- unlist(pal)
-
+  
 ocorPal <- which(str_detect(pal, "s[oóò]zinh[aos]") == T)
-
+  
 # Filtragem de sentenças no texto
 sen <- tokenize_sentences(x = textoTratado)
 sen <- unlist(sen)
@@ -35,5 +35,5 @@ sen <- unlist(sen)
 ocorSen <-which(str_detect(sen, "s[oóò]zinh[aos]") == T)
 
 # Transposição das sentenças para um dataframe
-dataSen <- data.frame()
-dataSen <- rbind(dataSen, data.frame(sentenças = sen[ocorSen]))
+dataSen <- data.frame(tipo = NULL, sentenças = NULL)
+dataSen <- rbind(dataSen, data.frame(tipo = "A", sentenças = sen[ocorSen]))
