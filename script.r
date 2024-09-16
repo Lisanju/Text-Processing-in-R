8c3bdbd2a475ba7bb807b941a71e911e7e6c4c47
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-09-16 13:02:13 -0300

    Update script.r

--- script.r
+++ script.r
@@ -46,3 +46,14 @@ ocorSen <-which(str_detect(sen, "s[oóò]zinh[aos]") == T)
 # Transposição das sentenças para um dataframe
 dataSen <- data.frame(tipo = NULL, sentenças = NULL)
 dataSen <- rbind(dataSen, data.frame(amostra = "B", sentenças = sen[ocorSen]))
+
+# Leitura da planilha rotulada
+arqRotulado <- read.table("/home/lisanju/Desktop/ProjetoElisa/planilha.txt", header = TRUE, sep = "\t", fill = TRUE, stringsAsFactors = FALSE, quote = "")
+  
+# Gráficos
+freqTable <- table(arqRotulado$amostra, arqRotulado$inter)
+barplot(freqTable, beside = FALSE, col = c("blue", "red"), 
+        legend.text = TRUE, args.legend = list(title = "Amostras"),
+        xlab = "Frequência das interpretações'", 
+        ylab = "Interpretações'",
+        main = "Quantificação de 'sozinho' em A e B")
